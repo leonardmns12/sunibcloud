042d6d38079954fac01f4c7a5847a2db139a9de7
leonardmns12 <54368620+leonardmns12@users.noreply.github.com>
2020-10-19 15:12:56 +0700

    trashbin list,adapter,fetching http method

---
+++ app/src/main/java/com/leydevelopment/sunibcloud/TrashbinActivity.java
@@ -0,0 +1,14 @@
+package com.leydevelopment.sunibcloud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TrashbinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trashbin);
+    }
+}
